7925116cf1170bdf4b93f969a81261afea58131d
Sebastian Grzelak <segrz8@gmail.com>
2020-01-04 16:37:28 +0100

    Up to iPad portrait

--- index.js
+++ index.js
@@ -2,7 +2,54 @@ const burger = document.querySelector('.fa-bars')
 const nav = document.querySelector('.nav')
 
 function openNav() {
-    nav.classList.toggle('nav--navOpen')
+    nav.classList.toggle('nav--open')
 }
 
 burger.addEventListener('click', openNav)
+
+class DiscographyOpener {
+    constructor() {
+        document.querySelector('.arrow-mgla').addEventListener('click', () => this.handleDiscography('mgla'))
+        document.querySelector('.arrow-ksm').addEventListener('click', () => this.handleDiscography('ksm'))
+        document.querySelector('.arrow-noSolace').addEventListener('click', () => this.handleDiscography('noSolace'))
+        document.querySelector('.arrow-ordersPoland').addEventListener('click', () => this.handleDiscography('ordersPoland'))
+
+        document.querySelector('.navMgla').addEventListener('click', () => this.handleNav('mgla'))
+        document.querySelector('.navKsm').addEventListener('click', () => this.handleNav('ksm'))
+        document.querySelector('.navNoSolace').addEventListener('click', () => this.handleNav('noSolace'))
+        document.querySelector('.navContact').addEventListener('click', () => this.handleNav('contact'))
+        document.querySelector('.navOrdersPoland').addEventListener('click', () => this.handleNav('ordersPoland'))
+    }
+
+    handleDiscography = (subject) => {
+        this.discographySubject = document.querySelector(`.${subject}__discography`)
+
+        this.discographySubject.classList.toggle(`${subject}__discography--open`)
+
+        window.scrollTo({
+            top: document.querySelector(`.arrow-${subject}`).offsetTop - 120,
+            behavior: 'smooth'
+        })
+
+        // if (!this.discographyOpen) {
+        //     window.scrollTo({
+        //         top: this.discographySubject.offsetTop - 120,
+        //         behavior: 'smooth'
+        //     })
+        // }
+
+        // this.discographyOpen = !this.discographyOpen
+    }
+    handleNav = (subject) => {
+        this.navSubject = document.querySelector(`.${subject}`)
+
+        window.scrollTo({
+            top: document.querySelector(`.${subject}`).offsetTop - 80,
+            behavior: 'smooth'
+        })
+
+        nav.classList.toggle('nav--open')
+    }
+}
+
+const discographyOpener = new DiscographyOpener()
