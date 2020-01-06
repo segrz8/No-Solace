45a5c86b696c85ba720dd51855d5e665380e3c61
Sebastian Grzelak <segrz8@gmail.com>
2020-01-06 20:25:40 +0100

    Remove useless variables

--- index.js
+++ index.js
@@ -21,22 +21,18 @@ class DiscographyOpener {
     }
 
     handleDiscography = (subject) => {
-        this.discographySubject = document.querySelector(`.${subject}__discography`)
-
-        this.discographySubject.classList.toggle(`${subject}__discography--open`)
+        document.querySelector(`.${subject}__discography`).classList.toggle(`${subject}__discography--open`)
 
         window.scrollTo({
             top: document.querySelector(`.arrow-${subject}`).offsetTop - 120,
-            behavior: 'smooth'
+            behavior: 'smooth',
         })
     }
 
     handleNav = (subject) => {
-        this.navSubject = document.querySelector(`.${subject}`)
-
         window.scrollTo({
             top: document.querySelector(`.${subject}`).offsetTop - 80,
-            behavior: 'smooth'
+            behavior: 'smooth',
         })
 
         nav.classList.toggle('nav--open')
